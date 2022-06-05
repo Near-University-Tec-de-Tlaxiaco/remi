10ae74f5674a3b5fa0907242c7c87f95440ecfae
Issa.Huerta <79929235+IsaHuerta23@users.noreply.github.com>
2022-06-04 19:40:57 -0500

    Actualizando index.ts

--- assembly/index.ts
+++ assembly/index.ts
@@ -13,6 +13,16 @@ export function setReseña(id: string, reseña: string): Reseñar {
     return newRsñ;
 }
 
+//Metodo para obtener todas las reseñas
+export function getTodasReseñas(): Reseñar[] {
+    const data = new Array<Reseñar>(indReseña);
+    for(let i = 0; i < indReseña; i++) {
+        data[i] = ReseñarTodo[i];
+        logging.log(i);
+    }
+    return data;
+}
+
 //Método para buscar reseñas
 export function getReseña(id: string): string | null {
     logging.log("Se ha encontrado una reseña");
