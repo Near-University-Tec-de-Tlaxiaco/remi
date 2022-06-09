31b02e0b743e174b6f5217f53200a0df741871f5
Issa.Huerta <79929235+IsaHuerta23@users.noreply.github.com>
2022-06-09 14:35:48 -0500

    Actualizar métodos del contrato.

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,38 +1,43 @@
 import {u128, logging, PersistentUnorderedMap} from "near-sdk-as";
-import {ReseñarTodo, Reseñar} from "./model"
+import {guardarReseña, Reseña, guardarPrestador, Prestador, guardarUsuario, Usuario} from "./model"
 
 export const reseñas = new PersistentUnorderedMap<string, string>("Reseñas");
 
-const indReseña = ReseñarTodo.length;
+const indReseña = guardarReseña.length;
 
 //Método para guardar reseñas
-export function setReseña(id: string, reseña: string): Reseñar {
-    const newRsñ = new Reseñar(id, reseña);
-    ReseñarTodo.push(newRsñ);
+export function setReseña(titulo : String, texto : String, calificacion : i32): Reseña {
+    const newRsñ = new Reseña(titulo, texto, calificacion);
+    guardarReseña.push(newRsñ);
     logging.log("Nueva reseña publicada");
     return newRsñ;
 }
 
 //Metodo para obtener todas las reseñas
-export function getTodasReseñas(): Reseñar[] {
-    const data = new Array<Reseñar>(indReseña);
+export function getTodasReseñas(): Reseña[] {
+    const data = new Array<Reseña>(indReseña);
     for(let i = 0; i < indReseña; i++) {
-        data[i] = ReseñarTodo[i];
+        data[i] = guardarReseña[i];
         logging.log(i);
     }
     return data;
+}
+
+//Método para borrar reseñas
+export function deleteReseña(reseñaIndice : i32): bool{
+    if(guardarReseña.length < reseñaIndice) {
+        logging.log('La reseña no existe.');
+        return false;
+    }
+    guardarReseña.swap_remove(reseñaIndice);
+    logging.log('Reseña eliminada.');
+    return true;
 }
 
 //Método para buscar reseñas
 export function getReseña(id: string): string | null {
     logging.log("Se ha encontrado una reseña");
     return reseñas.get(id);
-}
-
-//Método para borrar reseñas
-export function deleteReseña(id: string): void{
-    logging.log("Se ha borrado una reseña");
-    reseñas.delete(id);
 }
 
 //Metodo para obtener el numero de reseñas
